f4c9b5f5c8339960761faa514af709537c9b4265
Carlos Flores <carlos.flores@resuelve.mx>
2019-03-11 20:19:46 -0600

    Add message when update finished

--- src/electron/electron-app.js
+++ src/electron/electron-app.js
@@ -118,6 +118,7 @@ const checkForUpdates = () => {
           updater(() => {
             updating = false
             updateDownloaded = true
+            checkForUpdates()
           }, true)
         }
       })
